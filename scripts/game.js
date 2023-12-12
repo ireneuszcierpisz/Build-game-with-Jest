908e9d46ca77ed9b735bf8ab9f37598d99565722
ireneuszcierpisz <>
2023-12-12 10:34:21 +0000

    Add game object and a content to the game.test.js

---
+++ scripts/game.js
@@ -0,0 +1,10 @@
+let game = {
+    currentGame: [],
+    score: 0,
+    playerMoves: [],
+    choices: [],
+};
+
+
+module.exports = { game }; //because we'll be exporting more than  
+//one object and function from this file, so we need to put them in curly braces.
